5ffbaaf1a88281380d6aad7f85aca8ae4b38e7de
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-11-19 20:47:56 +0200

    Post and Get

---
+++ Electronics_store/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Electronics_store.Data;
+using Electronics_store.DTOs;
+using Electronics_store.Models;
+using Electronics_store.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Electronics_store.Controllers
+{
+    [Route("api/[controller]")] //[controller] -> inseamna numele controllerului
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly ElectronicsStoreContext _context;
+
+        public UserController(IUserService userService, ElectronicsStoreContext context)
+        {
+            _userService = userService;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetById(Guid Id)
+        {
+            var result = _userService.GetDataByUserId(Id);
+            return Ok(result);
+        }
+        
+        [HttpPost("create")]
+        public IActionResult Create(UserDTO user)
+        {
+            _userService.CreateUser(user);
+            //ar trebui sa folosim context ca sa adaugam userul in baza de date
+            return Ok();
+        }
+    }
+}
