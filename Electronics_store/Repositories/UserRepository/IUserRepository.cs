6927dc14ce9a2f4ff4746e0a3e936f13b90fca8c
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-11-19 20:51:15 +0200

    get user by id

---
+++ Electronics_store/Repositories/UserRepository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Electronics_store.DTOs;
+using Electronics_store.Models;
+using Electronics_store.Repositories.GenericRepository;
+
+namespace Electronics_store.Repositories.UserRepository
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        User GetById(Guid id);
+
+    }
+}
