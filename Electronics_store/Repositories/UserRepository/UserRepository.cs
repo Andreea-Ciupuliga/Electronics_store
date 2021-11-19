7cdc21dd906ab22911c4cd1aea35ba95816d150a
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-11-19 20:50:58 +0200

    get user by id

---
+++ Electronics_store/Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Electronics_store.Data;
+using Electronics_store.DTOs;
+using Electronics_store.Models;
+using Electronics_store.Repositories.GenericRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Electronics_store.Repositories.UserRepository
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        private readonly ElectronicsStoreContext _context;
+
+        public UserRepository(ElectronicsStoreContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public User GetById(Guid id)
+        {
+            return _table.FirstOrDefault(u => u.Id.Equals(id));
+        }
+
+    }
+}
