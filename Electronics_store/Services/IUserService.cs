ced904d762c23de581eb9c576bc39b92c069caad
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-11-19 20:48:50 +0200

    Create user and get user by id

---
+++ Electronics_store/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Electronics_store.DTOs;
+using Electronics_store.Models;
+
+namespace Electronics_store.Services
+{
+    public interface IUserService
+    {
+        UserDTO GetDataByUserId(Guid Id);
+        
+        void CreateUser(UserDTO entity);
+    }
+}
