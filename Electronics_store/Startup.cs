3c36348cc4703292d792d54fa0b5f731ec022c37
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-11-19 20:49:25 +0200

    add repositories and services

--- Electronics_store/Startup.cs
+++ Electronics_store/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Electronics_store.Data;
+using Electronics_store.Repositories.UserRepository;
+using Electronics_store.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace Electronics_store
@@ -27,7 +29,21 @@ namespace Electronics_store
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Electronics_store", Version = "v1" });
             });
+            //repositories
             
+            //se creeaza de fiecare data cand se face un request
+            services.AddTransient<IUserRepository, UserRepository>();
+            
+            //se creeaza cand se face primul request
+            //services.AddSingleton<IDatabaseRepository, DatabaseRepository>();
+            
+            //se creeaza o data per client requeat
+            //services.AddScoped<IDatabaseRepository, DatabaseRepository>();
+
+
+
+            //services
+            services.AddTransient<IUserService, UserService>();
             services.AddDbContext<ElectronicsStoreContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
         }
