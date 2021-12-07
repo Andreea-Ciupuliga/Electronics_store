e8fa12a4a01613bc9b9256ee8de0693c271fcd12
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-12-07 18:11:29 +0200

    Product Controller

---
+++ Electronics_store/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Electronics_store.Data;
+using Electronics_store.Models;
+using Electronics_store.Services.ProductService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Electronics_store.Controllers
+{
+    [Route("api/[controller]")] //[controller] -> inseamna numele controllerului
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        private readonly ElectronicsStoreContext _context;
+
+        public ProductController(IProductService productService,ElectronicsStoreContext context)
+        {
+            _productService = productService;
+            _context = context;
+        }
+        
+        //GET
+        [HttpGet("byId")]
+        public IActionResult GetById(Guid Id)
+        {
+            return Ok(_productService.GetProductByProductId(Id));
+        }
+        
+       
+        [HttpGet("allProducts")]
+        public IActionResult GetAllProducts() 
+        {
+            return Ok(_productService.GetAllProducts());
+        }
+        
+        
+        //POST
+        [HttpPost("create")]
+        public IActionResult Create(Product product)
+        {
+            _productService.CreateProduct(product);
+            return Ok();
+        }
+        
+        //PUT
+        [HttpPut("update")]
+        public IActionResult Update(Product product, Guid id)
+        {
+            _productService.UpdateProduct(product,id);
+            return Ok();
+        }
+        
+        
+        //DELETE
+        [HttpDelete]
+        public IActionResult DeleteById(Guid Id)
+        {
+            _productService.DeleteProductById(Id);
+            return Ok();
+        }
+    }
+}
