462ea6bbb069683dca528bf7a078d72b255721e5
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-12-07 16:35:13 +0200

    Category Controller

---
+++ Electronics_store/Controllers/CategoryController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Electronics_store.Data;
+using Electronics_store.Models;
+using Electronics_store.Services.CategoryService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Electronics_store.Controllers
+{
+    [Route("api/[controller]")] //[controller] -> inseamna numele controllerului
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly ElectronicsStoreContext _context;
+
+        public CategoryController(ICategoryService categoryService,ElectronicsStoreContext context)
+        {
+            _categoryService = categoryService;
+            _context = context;
+        }
+        
+        //GET
+        [HttpGet("byId")]
+        public IActionResult GetById(Guid Id)
+        {
+            return Ok(_categoryService.GetCategoryByCategoryId(Id));
+        }
+        
+       
+        [HttpGet("allCategories")]
+        public IActionResult GetAllCategories() 
+        {
+            return Ok(_categoryService.GetAllCategories());
+        }
+        
+        
+        //POST
+        [HttpPost("create")]
+        public IActionResult Create(Category category)
+        {
+            _categoryService.CreateCategory(category);
+            return Ok();
+        }
+        
+        //PUT
+        [HttpPut("update")]
+        public IActionResult Update(Category category, Guid id)
+        {
+            _categoryService.UpdateCategory(category,id);
+            return Ok();
+        }
+        
+        
+        //DELETE
+        [HttpDelete]
+        public IActionResult DeleteById(Guid Id)
+        {
+            _categoryService.DeleteCategoryById(Id);
+            return Ok();
+        }
+    }
+}
