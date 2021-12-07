7fcc1591da8236ae430a5e5cfe2ff402690030ec
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-12-07 18:12:20 +0200

    Product Repository

---
+++ Electronics_store/Repositories/ProductRepository/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using Electronics_store.Models;
+using Electronics_store.Repositories.GenericRepository;
+
+namespace Electronics_store.Repositories.ProductRepository
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        
+    }
+}
---
+++ Electronics_store/Repositories/ProductRepository/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Electronics_store.Data;
+using Electronics_store.Models;
+using Electronics_store.Repositories.GenericRepository;
+
+namespace Electronics_store.Repositories.ProductRepository
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        private readonly ElectronicsStoreContext _context;
+
+        public ProductRepository(ElectronicsStoreContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
