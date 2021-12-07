4b1dfbab5499dd999f8060369f5a3c978d3c2fd3
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-12-07 18:12:01 +0200

    add Json Ignore

--- Electronics_store/Models/Product.cs
+++ Electronics_store/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using Electronics_store.Models.Base;
 
 namespace Electronics_store.Models
@@ -9,9 +10,10 @@ namespace Electronics_store.Models
         public string Name { get; set; }
         public float Price { get; set; }
         public string Description { get; set; }
-        
+        [JsonIgnore]
         public ICollection<OrderProductRelation> OrderProductRelations { get; set; }
         
+        [JsonIgnore]
         public Category Category { get; set; }
         public Guid CategoryId { get; set; } //FK
     }
