3919ea5d231fe98f19c7a9a74fe02603bdb34bda
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-12-07 18:12:42 +0200

    Product Service

---
+++ Electronics_store/Services/ProductService/IProductService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Electronics_store.Models;
+
+namespace Electronics_store.Services.ProductService
+{
+    public interface IProductService
+    {
+        public Task<List<Product>> GetAllProducts();
+        
+        Product GetProductByProductId(Guid Id);
+        
+        void CreateProduct(Product entity);
+        
+        void DeleteProductById(Guid id);
+        
+        void UpdateProduct(Product product, Guid id);
+    }
+}
---
+++ Electronics_store/Services/ProductService/ProductService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Electronics_store.Models;
+using Electronics_store.Repositories.ProductRepository;
+
+namespace Electronics_store.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        public IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        
+        public Task<List<Product>> GetAllProducts()
+        {
+            Task<List<Product>> productsList =  _productRepository.GetAll();
+            return productsList;
+        }
+
+        public Product GetProductByProductId(Guid Id)
+        {
+            Product product =  _productRepository.FindById(Id);
+            return product;
+        }
+
+        public void CreateProduct(Product entity)
+        {
+            var productToCreate = new Product
+            {
+                Name = entity.Name,
+                Price = entity.Price,
+                Description = entity.Description,
+                CategoryId = entity.CategoryId,
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now
+            };
+
+            _productRepository.Create(productToCreate);
+            _productRepository.Save();
+        }
+
+        public void DeleteProductById(Guid id)
+        {
+            Product product =  _productRepository.FindById(id);
+            _productRepository.Delete(product);
+            _productRepository.Save();
+        }
+
+        public void UpdateProduct(Product product, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
