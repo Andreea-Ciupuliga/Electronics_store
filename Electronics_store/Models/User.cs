ef567c4fe460eb6e55ca75f6a164069fc19fad29
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-12-12 19:22:22 +0200

    Add JsonIgnore and role

--- Electronics_store/Models/User.cs
+++ Electronics_store/Models/User.cs
@@ -6,11 +6,15 @@ namespace Electronics_store.Models
 {
     public class User : BaseEntity
     {
+        public string PasswordHash { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
+        
+        [JsonIgnore]
         public ICollection<Order> Orders { get; set; }
+        
+        public Role Role { get; set; }
     }
 }
