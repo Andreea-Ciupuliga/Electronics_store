78d06d0b9496872a96a1b119185a23b346ab19db
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-12-12 20:48:40 +0200

    rename class and variable

--- Electronics_store/DTOs/UserRegisterDTO.cs
+++ Electronics_store/DTOs/UserRegisterDTO.cs
@@ -2,16 +2,12 @@
 
 namespace Electronics_store.DTOs
 {
-    public class RegisterUserDTO
+    public class UserRegisterDTO
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
-        
-        public DateTime? DateCreated { get; set; }
-        
-        public DateTime? DateModified { get; set; }
+        public string PasswordHash { get; set; }
     }
 }
