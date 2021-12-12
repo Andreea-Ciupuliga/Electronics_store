655f33b5fe7959f39cfd9c6b0966b25da8b42500
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-12-12 20:57:28 +0200

    MapperProfiles

---
+++ Electronics_store/Profiles/MapperProfiles.cs
@@ -0,0 +1,27 @@
+﻿using Electronics_store.DTOs;
+using Electronics_store.Models;
+using AutoMapper;
+
+namespace Electronics_store.Profiles
+{
+    public class MapperProfiles : Profile
+    {
+        public MapperProfiles()
+        {
+            //User
+            CreateMap<User, UserRespondDTO>();
+            CreateMap<User, UserRegisterDTO>();
+            CreateMap<UserRegisterDTO, User>();
+            CreateMap<User, UserLoginDTO>();
+            CreateMap<User, UserResponseTokenDTO>();
+            
+            //Category
+            CreateMap<Category, CategoryRegisterDTO>();
+            
+            //Product
+            CreateMap<Product, ProductRegisterDTO>();
+        }
+
+        
+    }
+}
