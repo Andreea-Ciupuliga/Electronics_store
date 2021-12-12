57800209e709e2598b3ddceaec83743de796d5ce
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-12-12 19:23:06 +0200

    add json ignore

--- Electronics_store/Models/OrderProductRelation.cs
+++ Electronics_store/Models/OrderProductRelation.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace Electronics_store.Models
 {
     public class OrderProductRelation
     {
         public Guid OrderId { get; set; }
+        [JsonIgnore]
         public Order Order { get; set; }
         
         public Guid ProductId { get; set; }
+        [JsonIgnore]
         public Product Product { get; set; }
     }
 }
