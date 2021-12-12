927712358959904e38de6139a0d7ca9bb758155e
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2021-12-12 20:49:10 +0200

    login dto

---
+++ Electronics_store/DTOs/UserLoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Electronics_store.DTOs
+{
+    public class UserLoginDTO
+    {
+        //aici sunt datele transmise de noi ca sa ne logam
+        
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+    }
+}
