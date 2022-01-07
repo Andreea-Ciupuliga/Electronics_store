558774672f8b09c32685b95ea003ead93af0f0e5
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-01-07 11:55:01 +0200

    order service

---
+++ Electronics_store/Services/OrderService/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Electronics_store.DTOs;
+using Electronics_store.Models;
+
+namespace Electronics_store.Services.OrderService
+{
+    public interface IOrderService
+    {
+        Order GetOrderByOrderId(Guid Id);
+        public List<Order> GetAllOrders();
+        void CreateOrder(OrderRegisterDTO entity);
+        void DeleteOrderById(Guid id);
+        void UpdateOrder(OrderUpdateDTO order, Guid id);
+        
+    }
+}
---
+++ Electronics_store/Services/OrderService/OrderService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Electronics_store.Data;
+using Electronics_store.DTOs;
+using Electronics_store.Models;
+using Electronics_store.Repositories.OrderRepository;
+using Microsoft.Data.SqlClient;
+
+namespace Electronics_store.Services.OrderService
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IMapper _mapper;
+        public ElectronicsStoreContext _context;
+        public IOrderRepository _orderRepository;
+
+
+        public OrderService(IMapper mapper, ElectronicsStoreContext context, IOrderRepository orderRepository)
+        {
+            _mapper = mapper;
+            _context = context;
+            _orderRepository = orderRepository;
+        }
+
+
+        public Order GetOrderByOrderId(Guid Id)
+        {
+            Order order = _orderRepository.FindById(Id);
+
+            if (order == null)
+                throw new Exception("Order not found");
+
+            return order;
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            List<Order> ordersList = _orderRepository.GetAllOrders();
+
+            if (ordersList.Count == 0)
+                throw new Exception("There are no orders");
+            
+            return ordersList;
+        }
+
+        public void CreateOrder(OrderRegisterDTO entity)
+        {
+            var orderToCreate = _mapper.Map<Order>(entity);
+            orderToCreate.DateCreated = DateTime.Now;
+            orderToCreate.DateModified = DateTime.Now;
+
+            _orderRepository.Create(orderToCreate);
+            _orderRepository.Save();
+        }
+
+        public void DeleteOrderById(Guid id)
+        {
+            Order order = _orderRepository.FindById(id);
+            
+            if (order == null)
+                throw new Exception("Order not found");
+            
+            _orderRepository.Delete(order);
+            _orderRepository.Save();
+        }
+
+        public void UpdateOrder(OrderUpdateDTO order, Guid id)
+        {
+            Order orderToUpdate = _orderRepository.FindById(id);
+            
+            if (orderToUpdate == null)
+                throw new Exception("Order not found");
+
+            orderToUpdate = _mapper.Map<OrderUpdateDTO, Order>(order, orderToUpdate);
+            orderToUpdate.DateModified = DateTime.Now;
+
+            try
+            {
+                _orderRepository.Update(orderToUpdate);
+                _orderRepository.Save();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
