1ff47c9e8994a6dc417602f8c91398de8e16282d
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-01-07 12:30:12 +0200

    OrderRegisterDTO

---
+++ Electronics_store/DTOs/OrderRegisterDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Electronics_store.DTOs
+{
+    public class OrderRegisterDTO
+    {
+        public float TotalPrice { get; set; }
+
+        public Guid UserId { get; set; } //FK
+        
+    }
+}
