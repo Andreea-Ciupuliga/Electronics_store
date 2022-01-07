18e2a4fe8a66dda35e52192b36d3f9a79dc642a7
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-01-07 11:55:31 +0200

    order repository

---
+++ Electronics_store/Repositories/OrderRepository/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Electronics_store.Models;
+using Electronics_store.Repositories.GenericRepository;
+
+namespace Electronics_store.Repositories.OrderRepository
+{
+    public interface IOrderRepository : IGenericRepository<Order>
+    {
+        List<Order> GetAllOrders();
+        
+    }
+}
---
+++ Electronics_store/Repositories/OrderRepository/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Electronics_store.Data;
+using Electronics_store.Models;
+using Microsoft.EntityFrameworkCore;
+using Electronics_store.Repositories.GenericRepository;
+
+namespace Electronics_store.Repositories.OrderRepository
+{
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
+    {
+        private readonly ElectronicsStoreContext _context;
+
+        public OrderRepository(ElectronicsStoreContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            return new List<Order>(_context.Orders.AsNoTracking().ToList());
+        }
+    }
+}
