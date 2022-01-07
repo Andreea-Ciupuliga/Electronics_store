57b83edd738ca0211d48e9674644539e62b49a3b
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-01-07 12:35:27 +0200

    DeliveryAddressRepository

---
+++ Electronics_store/Repositories/DeliveryAddressRepository/DeliveryAddressRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Electronics_store.Data;
+using Electronics_store.Models;
+using Electronics_store.Repositories.GenericRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Electronics_store.Repositories.DeliveryAddressRepository
+{
+    public class DeliveryAddressRepository : GenericRepository<DeliveryAddress>, IDeliveryAddressRepository
+    {
+        private readonly ElectronicsStoreContext _context;
+
+        public DeliveryAddressRepository(ElectronicsStoreContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public List<DeliveryAddress> GetAllDeliveryAddresses()
+        {
+            return new List<DeliveryAddress>(_context.DeliveryAddresses.AsNoTracking().ToList());
+        }
+    }
+}
---
+++ Electronics_store/Repositories/DeliveryAddressRepository/IDeliveryAddressRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Electronics_store.Models;
+using Electronics_store.Repositories.GenericRepository;
+
+namespace Electronics_store.Repositories.DeliveryAddressRepository
+{
+    public interface IDeliveryAddressRepository : IGenericRepository<DeliveryAddress>
+    {
+        List<DeliveryAddress> GetAllDeliveryAddresses();
+    }
+}
