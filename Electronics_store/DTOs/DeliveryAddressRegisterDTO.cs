ce81374094d1c1aa69c9ae35d302082ae5e0bd37
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-01-07 12:30:28 +0200

    DeliveryAddressRegisterDTO

---
+++ Electronics_store/DTOs/DeliveryAddressRegisterDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Electronics_store.DTOs
+{
+    public class DeliveryAddressRegisterDTO
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string ZIPCode { get; set; }
+        public Guid OrderId { get; set; }
+    }
+}
