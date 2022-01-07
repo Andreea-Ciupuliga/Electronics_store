f3f0ba8dfc0c081f67c8739f7dd26de497e55bc0
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-01-07 12:29:48 +0200

    ProductUpdateDTO

---
+++ Electronics_store/DTOs/ProductUpdateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Electronics_store.DTOs
+{
+    public class ProductUpdateDTO
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Description { get; set; }
+    }
+}
