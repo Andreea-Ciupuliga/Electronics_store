eec745231a81ac5a6b0adce14cb25b003082d8d4
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-01-11 11:58:29 +0200

    OrderProductRelationRegisterDTO

---
+++ Electronics_store/DTOs/OrderProductRelationRegisterDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Electronics_store.DTOs
+{
+    public class OrderProductRelationRegisterDTO
+    {
+        public Guid OrderId { get; set; }  //FK
+
+        public Guid ProductId { get; set; }   //FK
+       
+    }
+}
