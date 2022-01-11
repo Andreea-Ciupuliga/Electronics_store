38a366df062dcdd241dd346ed3b444f88d74cacf
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-01-11 11:53:17 +0200

    OrderProduct Relation Repository

---
+++ Electronics_store/Repositories/OrderProductRelationRepository/IOrderProductRelationRepository.cs
@@ -0,0 +1,10 @@
+﻿using Electronics_store.Models;
+using Electronics_store.Repositories.GenericRepository;
+
+namespace Electronics_store.Repositories.OrderProductRelationRepository
+{
+    public interface IOrderProductRelationRepository : IGenericRepository<OrderProductRelation>
+    {
+        
+    }
+}
---
+++ Electronics_store/Repositories/OrderProductRelationRepository/OrderProductRelationRepository.cs
@@ -0,0 +1,16 @@
+﻿using Electronics_store.Data;
+using Electronics_store.Models;
+using Electronics_store.Repositories.GenericRepository;
+
+namespace Electronics_store.Repositories.OrderProductRelationRepository
+{
+    public class OrderProductRelationRepository : GenericRepository<OrderProductRelation>, IOrderProductRelationRepository
+    {
+        private readonly ElectronicsStoreContext _context;
+
+        public OrderProductRelationRepository(ElectronicsStoreContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
