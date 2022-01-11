cb34ad708d358eaf7870381873787447b498f1bf
Andreea-Ciupuliga <80451641+Andreea-Ciupuliga@users.noreply.github.com>
2022-01-11 12:00:07 +0200

    OrderProduct Relation Controller

---
+++ Electronics_store/Controllers/OrderProductRelationController.cs
@@ -0,0 +1,31 @@
+﻿using Electronics_store.Data;
+using Electronics_store.DTOs;
+using Electronics_store.Services.OrderProductRelationService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Electronics_store.Controllers
+{
+    [Microsoft.AspNetCore.Components.Route("api/[controller]")] //[controller] -> inseamna numele controllerului
+    [ApiController]
+    public class OrderProductRelationController: ControllerBase
+    {
+        private readonly IOrderProductRelationService _orderProductRelationService;
+        private readonly ElectronicsStoreContext _context;
+
+        public OrderProductRelationController(IOrderProductRelationService orderProductRelationService, ElectronicsStoreContext context)
+        {
+            _orderProductRelationService = orderProductRelationService;
+            _context = context;
+        }
+        
+        
+        //POST
+        [HttpPost("create")]
+        public IActionResult Create(OrderProductRelationRegisterDTO orderProductRelation)
+        {
+            _orderProductRelationService.CreateOrderProductRelation(orderProductRelation);
+            return Ok();
+        }
+    }
+    
+}
